a0daf8c87afff110565140c573c70c2e22cd8338
zhujiao6666 <642160575@qq.com>
2022-10-19 20:23:24 +0800

    初始化提交

---
+++ typeidea/typeidea/settings/develop.py
@@ -0,0 +1,8 @@
+from typeidea.typeidea.settings.base import * #NOQA
+DEBUG = True
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
